7029fffb63f37639b7e68c4a294cf352cef2ef3b
Suraj Annadate <124056028+Suraj-Annadate@users.noreply.github.com>
2023-06-18 12:51:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function volume_sphere() {
     //Write your code here
-  
+  var radius = document.getElementById('radius').value
+	var volume = (4/3)*Math.PI*Math.pow(radius,3)
+	document.getElementById('volume')=volume.toFixed(2)
 } 
+document.getElementById('MyForm').addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  volume_sphere(); 
+});
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
