c2f60631029a9549422c161bcff9db86db01b334
Suraj Annadate <124056028+Suraj-Annadate@users.noreply.github.com>
2023-06-26 17:08:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function volume_sphere() {
     //Write your code here
+	var radius = parseFloat(document.getElementById("radius").value);
+
+  if (isNaN(radius)) {
+    document.getElementById("volume").value = "Invalid input";
+  } else {
+    var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    document.getElementById("volume").value = volume.toFixed(2);
+  }
   var radius = document.getElementById('radius').value
 	var volume = (4/3)*Math.PI*Math.pow(radius,3)
 	document.getElementById('volume')=volume.toFixed(2)
 } 
+// 
 document.getElementById('MyForm').addEventListener('submit', function(event) {
   event.preventDefault(); 
   volume_sphere(); 
